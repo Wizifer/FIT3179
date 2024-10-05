ddf75e5e5afdc0cec564ade2003d93d81ebd709a
Eric Yang Chen <eche0034@student.monash.edu>
2024-10-05 22:04:32 +1000

    Week 10 changes

--- Vega_lite_git/vega_lite_vis.js
+++ Vega_lite_git/vega_lite_vis.js
@@ -2,3 +2,8 @@ var vg_1 = "vega_lite_renewable_energy_map.json";
 vegaEmbed("#bar_chart", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_2 = "vega_lite_renewable_energy_graph.json";
+vegaEmbed("#area_chart", vg_2).then(function(result) {
+  // Access the Vega view instance as result.view
+}).catch(console.error);
