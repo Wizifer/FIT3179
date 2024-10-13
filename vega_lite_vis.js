6f7ff8829c1597842519f0454d8a4c702f800a9b
Eric Yang Chen <eche0034@student.monash.edu>
2024-10-13 22:41:44 +1100

    Finalised Data Visualisation 2 Changes

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,9 +1,13 @@
 var vg_1 = "vega_lite_renewable_energy_map.json";
-vegaEmbed("#bar_chart", vg_1).then(function(result) {
+vegaEmbed("#map_chart", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_2 = "vega_lite_renewable_energy_graph.json";
 vegaEmbed("#area_chart", vg_2).then(function(result) {
   // Access the Vega view instance as result.view
+}).catch(console.error);
+
+var vg_3 = "vega_lite_renewable_energy_percentage_change_graph";
+vegaEmbed("#bar_chart", vg_3).then(function(result) {
 }).catch(console.error);
